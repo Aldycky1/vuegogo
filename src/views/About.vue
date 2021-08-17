5e3b45e20414ba1e305f081a95400a89d47226c0
Aldycky1 <bagusaldycky@gmail.com>
2021-08-17 14:09:52 +0700

    updated

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>Hi! This is an about page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  name: "About",
+  components: {
+    // HelloWorld,
+  },
+};
+</script>
---
+++ src/views/Profile.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="profile">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <div class="media text-left">
+            <img src="../assets/user-pic.jpg" class="mr-3 user-pic" alt="..." />
+            <div class="media-body">
+              <h5 class="mt-0">Aldycky Bagus Witjaksana</h5>
+              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+              scelerisque ante sollicitudin.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  name: "Profile",
+  components: {},
+};
+</script>
+
+<style scoped>
+.user-pic {
+  border-radius: 50%;
+  height: 80px;
+  width: 80px;
+  object-fit: cover;
+}
+</style>
