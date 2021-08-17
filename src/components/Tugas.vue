52ec43f4702c6467f36022a8a0e9bfa63b4f88b6
Aldycky1 <bagusaldycky@gmail.com>
2021-08-17 17:59:01 +0700

    updated

---
+++ src/components/Tugas.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container mt-5">
+    <div class="row header p-3">
+      <div class="col-lg-12">
+        <h2>What I do today?</h2>
+      </div>
+    </div>
+    <div class="row content mt-3">
+      <div class="col-lg-12">
+        <div class="pekerjaan-rumah">
+          <div class="row">
+            <div
+              v-for="tugasSaya in tugas"
+              v-bind:key="tugasSaya.id"
+              class="col-lg-12 mb-2 text-left tugas"
+            >
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  onclick=""
+                  type="checkbox"
+                  value=""
+                  id="defaultCheck1"
+                />
+                <label
+                  class="form-check-label item"
+                  v-bind:class="{ 'item-completed': tugasSaya.isDone }"
+                  for="defaultCheck1"
+                >
+                  {{ tugasSaya.deskripsi }}
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tugas",
+  data: function() {
+    return {
+      tugas: [
+        {
+          id: 1,
+          deskripsi: "Mengerjakan tugas proyek kuliah",
+          isDone: false,
+        },
+        {
+          id: 2,
+          deskripsi: "Melanjutkan pembuatan project website",
+          isDone: false,
+        },
+        {
+          id: 3,
+          deskripsi: "Menonton film dan youtube",
+          isDone: false,
+        },
+        {
+          id: 4,
+          deskripsi: "Main game",
+          isDone: false,
+        },
+      ],
+    };
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+.header {
+  background: #1abc9c;
+}
+
+.header h2 {
+  margin: 0;
+}
+
+.tugas .item {
+  font-size: 18px;
+}
+
+.item-completed {
+  color: #1abc9c;
+}
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -6,14 +6,14 @@
           <div class="media text-left">
             <img src="../assets/user-pic.jpg" class="mr-3 user-pic" alt="..." />
             <div class="media-body">
-              <h5 class="mt-0">Aldycky Bagus Witjaksana</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-              scelerisque ante sollicitudin.
+              <h5 class="mt-0">{{ firstName }} {{ lastName }}</h5>
+              {{ bio }}
             </div>
           </div>
         </div>
       </div>
     </div>
+    <Tugas />
   </div>
 </template>
 
@@ -21,10 +21,21 @@
 // @ is an alias to /src
 
 import "bootstrap/dist/css/bootstrap.css";
+import Tugas from "../components/Tugas.vue";
 
 export default {
   name: "Profile",
-  components: {},
+  data: function() {
+    return {
+      firstName: "Aldycky",
+      lastName: "Bagus",
+      bio:
+        "Hello!! Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.",
+    };
+  },
+  components: {
+    Tugas,
+  },
 };
 </script>
 
